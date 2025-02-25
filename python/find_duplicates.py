e5145c00faac77711774c4fe955222f24491b246
Henry Shaw <wshaw@unca.edu>
2025-02-25 13:22:12 -0500

    add find_duplicates.py

---
+++ python/find_duplicates.py
@@ -0,0 +1,15 @@
+def find_duplicates_nested_loop(l: list) -> list:
+    dupes = [x for x in set(l) if l.count(x) > 1]
+    return dupes
+
+if __name__ == "__main__":
+    sample1 = [3, 7, 5, 6, 7, 4, 8, 5, 7, 66]
+    sample2 = [3, 5, 6, 4, 4, 5, 66, 6, 7, 6]
+    sample3 = [3, 0, 5, 1, 0]
+    sample4 = [3]
+    
+    print("Sample 1:", find_duplicates_nested_loop(sample1))
+    print("Sample 2:", find_duplicates_nested_loop(sample2))
+    print("Sample 3:", find_duplicates_nested_loop(sample3))
+    print("Sample 4:", find_duplicates_nested_loop(sample4))
+
